3f0595ef260f1462d3c5435d75ed844ede6ccddf
Krishi-02 <54003342+Krishi-02@users.noreply.github.com>
2020-11-16 10:32:43 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+function showtext(){
+    var text = document.getElementById("txtarea");
+    var showarea = document.getElementsByClassName("demo");
+    showarea.innerHTML=text.value;
+   }
+
+//    function myFunction() {
+//     var x = document.getElementById("txtarea").value;
+//     document.getElementById("demo").innerHTML = x;
+//   }
